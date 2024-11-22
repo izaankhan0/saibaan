6565eb9aba841cf43645502c54d69d5542d600e4
Izaankhan0 <izaankhan.x@gmail.com>
2024-11-22 23:40:35 +0500

    added blue animation to sections

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let flag = false;
 let nav = document.querySelector('.van')
 let line = document.querySelectorAll('.line')
+let sections = document.querySelectorAll('.section')
 function switch1() {
   if (flag == false) {
     nav.classList.add('show');
     line[0].classList.add('alter-1');
     line[1].classList.add('alter-2');
     line[2].classList.add('alter-3');
+    sections[0].classList.add('blur');
+    sections[1].classList.add('blur');
+    sections[2].classList.add('blur');
+    sections[3].classList.add('blur');
+    sections[4].classList.add('blur');
     flag = true;
   }
   else if (flag == true) {
@@ -14,6 +20,11 @@ function switch1() {
     line[0].classList.remove('alter-1');
     line[1].classList.remove('alter-2');
     line[2].classList.remove('alter-3');
+    sections[0].classList.remove('blur');
+    sections[1].classList.remove('blur');
+    sections[2].classList.remove('blur');
+    sections[3].classList.remove('blur');
+    sections[4].classList.remove('blur');
     flag = false;
   }
 }
